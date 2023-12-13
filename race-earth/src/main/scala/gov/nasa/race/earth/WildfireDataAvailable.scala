ab4ca192e6d999e2581983b6d51e235ffcf09f13
masonlee277 <46269622+masonlee277@users.noreply.github.com>
2023-12-13 12:33:45 -0800

    wda

--- race-earth/src/main/scala/gov/nasa/race/earth/WildfireDataAvailable.scala
+++ race-earth/src/main/scala/gov/nasa/race/earth/WildfireDataAvailable.scala
@@ -2,102 +2,73 @@ package gov.nasa.race.earth
 
 import gov.nasa.race.geo.GeoPosition
 import gov.nasa.race.uom.DateTime
-import java.io.File
-import scala.Option
+import java.io.File // the base of the file handling chain
+import gov.nasa.race.http.{FileRetrieved, HttpActor}
 
 // Added in defaulting None behavior (some will error and not return during API call )
 // Only Publish this once its fully formatted and ready to be sent to the client
-case class WildfireText(
-    audioFile: Option[File] = None, // Audio MP3 file (just text)
+// First case class with just the fireTextFile
+// I May actually just want to save the JSON file --> not actually parse these fields
+case class Coordinate(latitude: Double, longitude: Double)
 
-)
-
-case class WildfireFVInfo(
- // Written by the data locator (parsing MP3 + potential other text like social media)
-  audioFile: Option[File] = None, // Audio MP3 file (just text)
-
-  // Written By FireVoice API 
-  date: Option[DateTime] = None,
-  Incident_ID: Option[String] = None,
-  Call_ID: Option[String] = None,
-  Coordinates: Option[List[String]] = None, // Coordinates as List[String] with [latitude, longitude]
-  Incident_Report: Option[String] = None,
-  Severity_Rating: Option[String] = None,
-  Coordinate_Type: Option[String] = None,
-
-)
-
-
-case class WildfireDataAvailable(
-
-  // Files
-  // by default all the fields are val (not var) so they are immutable.
-  // var is potential source of race conditions (can be changed and dangerous)
-
-  // Written by the data locator (parsing MP3 + potential other text like social media)
-  audioFile: Option[File] = None, // Audio MP3 file (just text)
-
-  // Written By FireVoice API 
-  date: Option[DateTime] = None,
-  Incident_ID: Option[String] = None,
-  Call_ID: Option[String] = None,
-  Coordinates: Option[List[String]] = None, // Coordinates as List[String] with [latitude, longitude]
-  Incident_Report: Option[String] = None,
-  Severity_Rating: Option[String] = None,
-  Coordinate_Type: Option[String] = None,
-
-  // Written By CloudFire Actor
-  simReport: Option[String] = None, //metadata for the simulation (num sims, etc)
-  fireFile: Option[File] = None, // GeoJSON file saved on local drive (only send fileName to the client, service knows where the file is stored)
-  
-) {
-
-  // TODO: WE CANNOT PASS DOWN THE PHYSICAL FILE TO THE CLIENT
-  // we just just send the filename and common prefix: /fire-perim/fireFileName
+// Second case class extending WildfireDataUnstructuredText with additional fields from the FireVoice API
+// Modify the WildfireGeolocationData case class
+case class WildfireGeolocationData(
+                                    fireTextFile: Option[File] = None,
+                                    date: Option[DateTime] = None,
+                                    Incident_ID: Option[String] = None,
+                                    Call_ID: Option[String] = None,
+                                    Coordinates: Option[List[Coordinate]] = None, // Now Coordinates is a List of Coordinate objects
+                                    Incident_Report: Option[String] = None,
+                                    Severity_Rating: Option[String] = None,
+                                    Coordinate_Type: Option[String] = None
+                                  ) {
   def toJson(): String = {
-    val coordJson = Coordinates.map(c => s""" "$c" """).mkString("[", ",", "]")
+    // Serialize Coordinates as a list of JSON objects
+    val coordJson = Coordinates.map(_.map(coord => s"""{"latitude": ${coord.latitude}, "longitude": ${coord.longitude}}""").mkString("[", ",", "]")).getOrElse("[]")
     s"""{
-      |  "audioFile": "${audioFile.map(_.getPath).getOrElse("N/A")}",
-      |  "fireFile": "${fireFile.map(_.getPath).getOrElse("N/A")}",
-      |  "date": ${date.map(_.toEpochMillis).getOrElse("N/A")},
-      |  "Incident_ID": "${Incident_ID.getOrElse("N/A")}",
-      |  "Call_ID": "${Call_ID.getOrElse("N/A")}",
-      |  "Coordinates": $coordJson,
-      |  "Incident_Report": "${Incident_Report.getOrElse("N/A")}",
-      |  "Severity_Rating": "${Severity_Rating.getOrElse("N/A")}",
-      |  "Coordinate_Type": "${Coordinate_Type.getOrElse("N/A")}",
-      |  "simReport": "${simReport.getOrElse("N/A")}"
-      |}""".stripMargin
+       |  "date": "${date.map(_.toString).getOrElse("N/A")}",
+       |  "Incident_ID": "${Incident_ID.getOrElse("N/A")}",
+       |  "Call_ID": "${Call_ID.getOrElse("N/A")}",
+       |  "Coordinates": $coordJson,
+       |  "Incident_Report": "${Incident_Report.getOrElse("N/A")}",
+       |  "Severity_Rating": "${Severity_Rating.getOrElse("N/A")}",
+       |  "Coordinate_Type": "${Coordinate_Type.getOrElse("N/A")}"
+       |}""".stripMargin
+  }
+}
+
+// Third case class that is the complete WildfireDataAvailable, including all previous data plus the CloudFire Actor data
+case class WildfireDataAvailable(
+                                  WildfireGeolocationData: WildfireGeolocationData,
+                                  simReport: Option[String] = None, // Metadata for the simulation (num sims, etc)
+                                  firePerimFile: Option[File] = None // GeoJSON file saved on local drive
+                                ) {
+  // TODO: We actually don't want to expose the backend datalocations to the frontend
+  // Take in arguments for the route where the frontend can access the files, right now its the actual filepath and this is wrong
+
+  def toJsonWithTwoUrls(fireTextUrl: String, firePerimUrl: String, id: String): String = {
+    val geolocationJson = WildfireGeolocationData.toJson().dropRight(1)
+    // Wrap in a fireVoiceLayer identifier so that the frontend understands the message and handles rendering apprioately
+    s"""{
+       |  "fireVoiceLayer": {
+       |    "id": "$id",
+       |    $geolocationJson,
+       |    "fireTextUrl": "$fireTextUrl",
+       |    "firePerimUrl": "$firePerimUrl",
+       |    "simReportUrl": "${simReport.getOrElse("N/A")}"
+       |  }
+       |}""".stripMargin
   }
 
-  def toJsonWithUrl(fireUrl: Option[String]): String = {
-    val coordJson = Coordinates.map(c => s""" "$c" """).mkString("[", ",", "]")
+
+  def toJsonWithUrl (url: String ): String = {
+    // Used for Layer HashMap that will serve individual files (can be any file). the id will be the call_id-incident_id
+    // and will be set in the FireVoiceService Actor
     s"""{
-      |  "audioFile": "${audioFile.map(_.getPath).getOrElse("N/A")}",
-      |  "fireFile": "${fireUrl.getOrElse("N/A")}",
-      |  "date": ${date.map(_.toEpochMillis).getOrElse("N/A")},
-      |  "Incident_ID": "${Incident_ID.getOrElse("N/A")}",
-      |  "Call_ID": "${Call_ID.getOrElse("N/A")}",
-      |  "Coordinates": $coordJson,
-      |  "Incident_Report": "${Incident_Report.getOrElse("N/A")}",
-      |  "Severity_Rating": "${Severity_Rating.getOrElse("N/A")}",
-      |  "Coordinate_Type": "${Coordinate_Type.getOrElse("N/A")}",
-      |  "simReport": "${simReport.getOrElse("N/A")}"
-      |}""".stripMargin
-  }
-  def toJsonWithTwoUrls(audioUrl: Option[String], fireUrl: Option[String]): String = {
-    val coordJson = Coordinates.map(c => s""" "$c" """).mkString("[", ",", "]")
-    s"""{
-      |  "audioFile": "${audioUrl.getOrElse("N/A")}",
-      |  "fireFile": "${fireUrl.getOrElse("N/A")}",
-      |  "date": ${date.map(_.toEpochMillis).getOrElse("N/A")},
-      |  "Incident_ID": "${Incident_ID.getOrElse("N/A")}",
-      |  "Call_ID": "${Call_ID.getOrElse("N/A")}",
-      |  "Coordinates": $coordJson,
-      |  "Incident_Report": "${Incident_Report.getOrElse("N/A")}",
-      |  "Severity_Rating": "${Severity_Rating.getOrElse("N/A")}",
-      |  "Coordinate_Type": "${Coordinate_Type.getOrElse("N/A")}",
-      |  "simReport": "${simReport.getOrElse("N/A")}"
-      |}""".stripMargin
+       |  "fireVoiceLayer": {
+       |     "url":"$url"
+       |  }
+       |}""".stripMargin
   }
 }
